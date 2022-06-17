3cfa31f393d4085deeba23651b3de3e9f545670f
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-17 07:43:57 +0300

    File create

---
+++ src/componnets/App.js
@@ -0,0 +1,13 @@
+import PostList from "./PostList";
+import UserList from "./UserList";
+
+function App() {
+  return (
+    <div>
+      <PostList />
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
